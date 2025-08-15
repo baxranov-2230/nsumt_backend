e6604671e82550dd6989581bdf9a36011dcc230a
Ahror <axrorwillberich2230@gmail.com>
2025-08-15 14:59:19 +0500

    Department page

--- src/api/v1/department_page/__init__.py
+++ src/api/v1/department_page/__init__.py
@@ -6,10 +6,7 @@ from src.api.v1.department_page.get_detail import router as get_detail_router
 from src.api.v1.department_page.delete import router as delete_router
 from src.api.v1.department_page.update import router as update_router
 
-
 department_page_router = APIRouter(prefix='/department_page', tags=['Department Page'])
-
-
 
 department_page_router.include_router(add_router)
 department_page_router.include_router(get_all_router)
