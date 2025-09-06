3feb44f6e375440b97ea3462f16dbd9b57770451
baxranov-2230 <axrorwillberich2230@gmail.com>
2025-09-06 11:38:27 +0500

    upload 3

--- src/api/v1/services/upload_flie.py
+++ src/api/v1/services/upload_flie.py
@@ -13,32 +13,25 @@ from dotenv import load_dotenv
 router = APIRouter()
 
 
-
-@router.post('/upload')
-# @has_access(roles=['admin'])
+@router.post("/upload")
 async def upload_file_func(
-        #upload_file: UploadFile = File(None),
-        # current_user: User = Depends(get_current_user),
-        request: Request = None,
-        db: AsyncSession = Depends(get_db)):
-
-    form_data = await request.form()
-    upload_file = form_data.get("upload_file")
-    if not upload_file:
-        raise HTTPException(status_code=400, detail="Fayl yuborilmadi")
-
+    upload_file: UploadFile = File(...),   # frontend "upload_file" nomi bilan yuboradi
+    db: AsyncSession = Depends(get_db)
+):
     try:
-        file_path = await save_file_upload(upload_file) if upload_file else None
-        new_file = Uploads(
-            upload_file=file_path
-        )
+        # Faylni saqlash
+        file_path = await save_file_upload(upload_file)
+
+        # Bazaga yozish
+        new_file = Uploads(upload_file=file_path)
         db.add(new_file)
         await db.commit()
         await db.refresh(new_file)
-        # file_url = f"http://127.0.0.2:8000/{file_path}"
-        # file_url = f"http://zaynidinov.uz:51432/{file_path}"
+
+        # Faylni URL qilib qaytarish
         file_url = f"{os.getenv('URL')}/{file_path}"
-        # return {"message": "File muvaffaqiyatli yaratildi"}
+
         return JSONResponse(content={"file_url": file_url}, status_code=200)
+
     except Exception as e:
-        return {"Yuklashda xatolik"}
+        raise HTTPException(status_code=500, detail=f"Fayl yuklashda xatolik: {str(e)}")
