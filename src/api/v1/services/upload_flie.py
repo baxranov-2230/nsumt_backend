3fe3531923c55f5946a32ff962fefd93da9097b4
baxranov-2230 <axrorwillberich2230@gmail.com>
2025-09-06 10:35:54 +0500

    upload 2

--- src/api/v1/services/upload_flie.py
+++ src/api/v1/services/upload_flie.py
@@ -13,19 +13,32 @@ from dotenv import load_dotenv
 router = APIRouter()
 
 
+
 @router.post('/upload')
+# @has_access(roles=['admin'])
 async def upload_file_func(
-        file: UploadFile = File(...),
+        #upload_file: UploadFile = File(None),
+        # current_user: User = Depends(get_current_user),
+        request: Request = None,
         db: AsyncSession = Depends(get_db)):
 
+    form_data = await request.form()
+    upload_file = form_data.get("upload_file")
+    if not upload_file:
+        raise HTTPException(status_code=400, detail="Fayl yuborilmadi")
+
     try:
-        file_path = await save_file_upload(file)
-        new_file = Uploads(upload_file=file_path)
+        file_path = await save_file_upload(upload_file) if upload_file else None
+        new_file = Uploads(
+            upload_file=file_path
+        )
         db.add(new_file)
         await db.commit()
         await db.refresh(new_file)
-
+        # file_url = f"http://127.0.0.2:8000/{file_path}"
+        # file_url = f"http://zaynidinov.uz:51432/{file_path}"
         file_url = f"{os.getenv('URL')}/{file_path}"
+        # return {"message": "File muvaffaqiyatli yaratildi"}
         return JSONResponse(content={"file_url": file_url}, status_code=200)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Yuklashda xatolik: {e}")
+        return {"Yuklashda xatolik"}
